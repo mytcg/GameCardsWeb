8b607bece8d2bb5c51133fa77b86501de9b1ba9c
genesis101 <cole@mytcg.net>
2012-09-04 11:44:53 +0200

    mxit files
    Signed-off-by: genesis101 <cole@mytcg.net>

---
+++ mxit/index.php
@@ -0,0 +1,11 @@
+<html>
+   <head>
+   <style>a {font-size:12px;font-family:"Arial","Arial Black";font-weight:900;text-decoration:none;color:#777777}</style>
+      <title>SA Rugby Cards Mxit App</title>
+   </head>
+   <body>
+       <img src="images/header_left.png" border="0" /><br />
+       <a href="info.php">Purchase Premium Credits with Moola</a><br />
+       <a href="http://sarugbycards.com/mobi" onclick="window.open(this.href); return false;">Back to SA Rugby Cards</a>
+   </body>
+<html>
---
+++ mxit/purchase.php
@@ -0,0 +1,22 @@
+<?php 
+require_once("conn.php");
+
+?>
+<html>
+   <head>
+   <style>p,a {font-size:12px;font-family:"Arial","Arial Black";font-weight:900;text-decoration:none;color:#777777}</style>
+      <title>SA Rugby Cards Mxit App</title>
+   </head>
+   <body>
+       <img src="images/header_left.png" border="0" /><br />
+       <?php if ($_SESSION['userID']){?>
+       <a href="confirm.php?a=500">350 Credits for 500 Moola</a><br />
+       <a href="confirm.php?a=1000">700 Credits for 1000 Moola</a><br />
+       <a href="confirm.php?a=2000">1400 Credits for 2000 Moola</a>
+       <br /><br />
+       <?php }else{ echo 'No user to assign credits, <a href="info.php">try again?</a><br />';} ?>
+       <a href="index.php">Back</a>
+   </body>
+
+<html>
+
